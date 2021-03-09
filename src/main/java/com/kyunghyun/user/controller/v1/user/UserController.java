e905b0c00eb681581af20b3d9b05093fb28c6969
kyunghyunkim <kyunghyun@terafunding.com>
2021-03-09 17:10:38 +0900

    AllArgsConstructor 어노테이션 적용

--- src/main/java/com/kyunghyun/user/controller/v1/user/UserController.java
+++ src/main/java/com/kyunghyun/user/controller/v1/user/UserController.java
@@ -2,20 +2,18 @@ package com.kyunghyun.user.controller.v1.user;
 
 import com.kyunghyun.user.dto.UserDto;
 import com.kyunghyun.user.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/v1/user")
 public class UserController {
 
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping(value = "/{userNo}")
     public ResponseEntity<UserDto> userInfo(@PathVariable("userNo") long userNo){
